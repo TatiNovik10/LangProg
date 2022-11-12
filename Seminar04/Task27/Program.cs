4584a701edc0325648485d41e02ba992113492d8
Tati <waterstone10proton.mail.com>
2022-11-13 00:59:06 +0300

    :Task25,Task27

---
+++ Seminar04/Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 27: Напишите функцию и запустите ее, которая
+//принимает на вход число и выдаёт сумму цифр в числе.
+int GetSum(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int sum = GetSum(N);
+Console.WriteLine($"Сумма цифр числа равна {sum}.");
+
+
+
+
+
