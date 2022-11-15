7a341b0e32dd2ebff3e9c421b1367ae04274f85c
Tati <waterstone10proton.mail.com>
2022-11-15 22:29:40 +0300

    :Task29

---
+++ Seminar04/Task29/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8
+//элементов и выводит их на экран.
+
+ Console.Write("Введите восемь чисел: ");
+    int[] array = new int [8];
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        Console.Write($"{array[i]} ");
+
+    }
+   
+    Console.Write(String.Join(",", array));
+
+
