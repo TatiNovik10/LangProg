cd90c8ba0e0faa20d9cd5e61dfcbe41521de42ba
Tatiana <novik10tanit@yandex.ru>
2022-11-19 21:31:21 +0300

    Seminar05

---
+++ Seminar04/Task29a/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8
+//элементов и выводит их на экран.(элементы массива вводятся вручную)
+
+void FillArray(int[] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+} 
+Console.WriteLine("Введите элементы массива:");
+int[] array = new int[8];
+FillArray(array);
+Console.WriteLine(String.Join(",", array));
---
+++ Seminar05/Task34/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте массив, заполненный случайными положительными трехзначными числами. Напишите программу,
+// которая покажет количество четных чисел в массиве.
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(100, 1000);
+    }
+    return res;
+}
+int GetEvenNum(int[] array)
+{
+    int countNum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 0)
+        {
+            countNum += 1;
+        }
+    }
+    return countNum;
+}
+int[] array = GetArray(25, 100, 999);
+Console.WriteLine(String.Join(" ,", array));
+int countNum2 = GetEvenNum(array);
+Console.WriteLine($"{countNum2}");
